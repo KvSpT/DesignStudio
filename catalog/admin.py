4946f4289b6efd5a7f93c7cae8786e97bfdedebb
Maksim Sh <maksims42sch@gmail.com>
2023-11-19 22:04:10 +0700

    add new models in admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import User, Project
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_created', 'designer', 'status', 'space_category', 'project_category')
+    list_editable = ('status', 'space_category', 'project_category')
+    list_filter = ('status', 'date_created')
+    def has_add_permission(self, request):
+        return False
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('initials', 'login', 'email')
+    def has_add_permission(self, request):
+        return False
+    def has_change_permission(self, request):
+        return False
