d9fefc717989614046a220d0bc018a9eaf311f98
Maksim Sh <maksims42sch@gmail.com>
2023-11-20 22:41:55 +0700

    create home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Project, User
 
-# Create your views here.
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_working_projects=Project.objects.filter(status__exact='w').count()
+    new_projects=Project.objects.filter(status__exact='n')
+
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_working_projects':num_working_projects,'new_projects':new_projects},
+    )
